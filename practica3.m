ce50ff4b12b647a27d312bfc4f2288df77c2b95d
Christian Torres Gonzalez <56066682+ChristianTorresGonzalez@users.noreply.github.com>
2020-02-27 14:21:11 +0000

    Practica en principio terminada

--- practica3.m
+++ practica3.m
@@ -12,4 +12,16 @@ practica = addmf(practica, 'output', 1, 'Abrir', 'trapmf', [0 50 100 150]);
 plotmf(practica, 'input', 1);
 plotmf(practica, 'output', 1);
 
-practica = addrule(practica, [1 2 1 1])
+practica = addrule(practica, [1 1 1 1 ; 2 1 1 1 ; 3 2 1 1]);
+showrule(practica);
+
+altura_actual = 19;
+altura_deseada = 20;
+error = altura_deseada - altura_actual;
+lado = 10;
+base = lado * lado;
+
+output = evalfis(error, practica)
+porcentaje_real = ((0.4 * output) / 100) - 0.1
+
+altura_final = altura_actual - (porcentaje_real / base)
